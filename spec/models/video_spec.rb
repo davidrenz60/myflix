d51866f481658987ca82b769371a0c82433580a2
David Renz <davidrenz60@gmail.com>
2017-09-10 14:23:23 -0700

    more tests for Video and Category models

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,20 @@ describe Video do
     video.save
     expect(Video.first).to eq(video)
   end
+
+  it "belongs to a cateogry" do
+    comedy = Category.create(name: "Comedy")
+    video = Video.create(title: "Family Guy", description: "family guy description.", category: comedy)
+    expect(video.category).to eq(comedy)
+  end
+
+  it "validates a title" do
+    video = Video.create(description: "test description.")
+    expect(Video.count).to eq(0)
+  end
+
+  it "validates a description" do
+    video = Video.create(title: "South Park")
+    expect(Video.count).to eq(0)
+  end
 end
