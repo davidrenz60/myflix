b293cefff118684b2f6a2a2488926c18fbda31fd
David Renz <davidrenz60@gmail.com>
2017-10-26 09:08:11 -0700

    mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,14 @@
 Myflix::Application.configure do
-  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'yourapp.heroku.com',
+    authentication:  :plain
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
 
   config.cache_classes = true
   config.eager_load = true
